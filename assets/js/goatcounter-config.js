528953239394b9d2d88f827efa9f59a92e5f68a9
vipyctmall <vipyctmall@gmail.com>
2026-07-22 15:49:18 +0800

    Create goatcounter-config.js

---
+++ assets/js/goatcounter-config.js
@@ -0,0 +1,9 @@
+/**
+ * AKG 訪客統計設定
+ * 1. 到 https://www.goatcounter.com 建立站台。
+ * 2. 將下方 CHANGE-ME 替換成你的 GoatCounter code（不含 .goatcounter.com）。
+ *    例如：https://akg-global.goatcounter.com -> code 為 akg-global
+ */
+window.AKG_STATS = Object.freeze({
+  goatCounterCode: "CHANGE-ME"
+});
